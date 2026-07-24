0ae3a7c5c698317efe91d34d731ebcf5dce4275c
SebaRomero <sebaromero.luthier@gmail.com>
2026-07-24 10:01:30 -0300

    upadate datos

--- combate/data/characters.js
+++ combate/data/characters.js
@@ -10,11 +10,11 @@ const characters = [
     evasion: 5,
     skills: [
       createSkill({ name: "Corte grave",           type: "attack",  power: 22, precision: 80, aparicion: 70, herida: true }),
-      createSkill({ name: "Golpe de escudo",        type: "attack",  power: 10, precision: 90, aparicion: 8,  stun: true  }),
-      createSkill({ name: "Plegaria",               type: "cura",    power: 15, precision: 95, aparicion: 4                   }),
-      createSkill({ name: "Postura Defensiva",      type: "defense", power: 35, precision: 95, aparicion: 6                   }),
+      createSkill({ name: "Golpe de escudo",        type: "attack",  power: 10, precision: 90, aparicion: 20,  stun: true  }),
+      createSkill({ name: "Plegaria",               type: "cura",    power: 15, precision: 95, aparicion: 20   }),
+      createSkill({ name: "Postura Defensiva",      type: "defense", power: 35, precision: 95, aparicion: 40   }),
       createSkill({ name: "Furia de batalla",       type: "buff",    target: "self",  stat: "attack",    value: 0.10,  precision: 95, aparicion: 7 }),
-      createSkill({ name: "Proteccion Divina",      type: "buff",    target: "self",  stat: "defense",   value: 10,    precision: 95, aparicion: 7  }),
+      createSkill({ name: "Proteccion Divina",      type: "buff",    target: "self",  stat: "defense",   value: 10,    precision: 95, aparicion: 50  }),
       createSkill({ name: "Oración de Precisión",   type: "buff",    target: "self",  stat: "precision", value: 100,   precision: 90, aparicion: 5 }),
     ]
   }),
